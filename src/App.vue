984ec611c32dfb3f91d2b103074ee01689a6392b
igprad <ign.aldi.pradana@gmail.com>
2017-05-14 14:58:59 +0700

    Added spa and try using vue data method etc

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">BliHire</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li v-on:click="chStatePageToFpk()"><router-link to="/fpk">FPK</router-link></li>
+          <li v-on:click="statePage = 'mpp'"><router-link to="/mpp">MPP</router-link></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li class=""><a href="#">Notification</a></li>
+          <li class=""><a href="#">Logout</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="navbar navbar-inverse navbar-fixed-left">
+      <ul class="nav navbar-nav">
+        <li><router-link :to="'/'+statePage+'/technology'">Technology</router-link></li>
+        <li><router-link :to="'/'+statePage+'/hrd'">Human Resource</router-link></li>
+      </ul>
+    </div>
+    <!-- <img src="./assets/logo.png"> -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      statePage: 'fpk'
+    }
+  },
+  methods: {
+    chStatePageToMpp () {
+      this.statePage = 'mpp'
+      alert(this.statePage)
+    },
+    chStatePageToFpk () {
+      this.statePage = 'fpk'
+      console.log(this.statePage)
+    }
+  }
 }
 </script>
 
@@ -18,6 +54,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+
+.navbar-fixed-left {
+  margin-top: -20px;
+  width: 140px;
+  position: fixed;
+  border-radius: 0;
+  height: 100%;
+}
+
+.navbar-fixed-left .navbar-nav > li {
+  float: none;  /* Cancel default li float: left */
+  width: 139px;
+}
+
+.navbar-fixed-left + .container {
+  padding-left: 160px;
+}
+
+/* On using dropdown menu (To right shift popuped) */
+.navbar-fixed-left .navbar-nav > li > .dropdown-menu {
+  margin-top: -50px;
+  margin-left: 140px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Technology from '@/components/Technology'
+import HRD from '@/components/HumanResource'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/fpk/Technology',
+      name: 'Technology',
+      component: Technology
+    },
+    {
+      path: '/fpk/hrd',
+      name: 'Hello',
+      component: HRD
     }
+
   ]
 })
