1d11d9eccada862592d352a7b1178585ef4a1d7e
igprad <ign.aldi.pradana@gmail.com>
2017-06-20 06:45:29 +0700

    init for vue : view cv, requeste fpk, hiring process in HRD

---
+++ src/components/hrd/ViewCV.vue
@@ -0,0 +1,444 @@
+<template>
+  <div class="listContent col-md-12">
+
+  <h2 style="text-align:center;">Formulir Lamaran</h2>
+  <br/>
+
+  <div class="list-group">
+      <a class="list-group-item" data-toggle="collapse" href="#position">Position Applied</a>
+    </div>
+    <div id="position" class="collapse container">
+    <table class="table table-bordered table-condensed">
+        <tr>
+            <th>Position Applied</th>
+            <td>Marketing</td>
+        </tr>
+    </table>
+  </div>
+    <div class="list-group">
+      <a class="list-group-item" data-toggle="collapse" href="#data-pribadi">I. Data Pribadi</a>
+    </div>
+    <div id="data-pribadi" class="collapse container">
+    <table class="table table-bordered table-condensed">
+        <tr>
+          <th>Nama lengkap</th>
+          <td>Alfons Manggala</td>
+        </tr>
+      <tr>
+        <th>Tempat Tanggal Lahir</th>
+        <td>30 Maret 1992</td>
+      </tr>
+
+      <tr>
+      <th>KTP</th>
+      <td>blablablabllabalbabl</td>
+      </tr>
+
+      <tr>
+        <th>SIM C/A/B1</th>
+        <td>xxxxxxxxxxxxxxx</td>
+        </tr>
+
+     <tr>
+        <th>Email</th>
+        <td>alfons32@gmail.com</td>
+      </tr>
+
+      <tr>
+        <th>Linkedin</th>
+        <td>AlfonsManggala</td>
+      </tr>
+
+      <tr>
+        <th>Telepon Seluler</th>
+        <td>+62-xxx-xx-xxxx-xx</td>
+      </tr>
+
+      <tr>
+        <th>Agama</th>
+        <td>Protestan</td>
+      </tr>
+
+      <tr>
+        <th>Suku Bangsa</th>
+        <td>Chineese</td>
+      </tr>
+
+      <tr>
+        <th>Status Pernikahan</th>
+        <td>Lajang</td>
+      </tr>
+
+      <tr>
+        <th>Alamat Saat ini</th>
+        <td>Jakarta</td>
+      </tr>
+
+
+      <tr>
+        <th>Telepon Rumah</th>
+        <td>-</td>
+      </tr>
+
+      <tr>
+        <th>Kontak Darurat</th>
+        <td>+6289660553886</td>
+      </tr>
+      </table>
+      </div>
+
+    <div class="list-group">
+      <a class="list-group-item" data-toggle="collapse" href="#data-keluarga">II.Latar Belakang Keluarga</a>
+    </div>
+    <div id="data-keluarga" class="collapse container">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Hubungan</th>
+            <th>Nama</th>
+            <th>Tanggal Lahir</th>
+            <th>Pendidikan Terakhir</th>
+            <th>Pekerjaan</th>
+          </tr>
+        </thead>
+      <tbody>
+        <tr>
+          <th>Ayah</th>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+          <th>Ibu</th>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+          <th rowspan=5 style="text-align: center;">Saudara</th>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+        <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+        <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+        <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+        <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+
+        <tr>
+          <th>Pasangan</th>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+
+        <tr>
+          <th rowspan=5>Anak</th>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+        <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+        <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+        <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+        <tr>
+        <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+          <td>Blalalaala</td>
+        </tr>
+
+        <tr>
+          <th>Tanggungan selain anak dan istrri</th>
+          <td colspan=5>Tidak</td>
+        </tr>
+      </tbody>
+      </table>
+    </div>
+
+    <div class="list-group">
+      <a class="list-group-item" data-toggle="collapse" href="#data-pendidikan">III. Latar Belakang Pendidikan dan Kemampuan Berbahasa</a>
+    </div>
+    <div id="data-pendidikan" class="collapse container">
+    <h4>1. Pendidikan Formal</h4>
+      <table class="table table-bordered">
+      <thead>
+        <tr>
+            <th>Pendidikan</th>
+            <th>Sekolah/Perguruan Tinggi</th>
+            <th>Jurusan</th>
+            <th>Kota</th>
+            <th>Periode</th>
+            <th>IPK</th>
+          </tr>
+        </thead>
+          <tbody>
+            <tr>
+              <td>SLTA</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <td>Diploma</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <td>S1</td>
+              <td>Ma Chung</td>
+              <td>Informatika</td>
+              <td>Malang</td>
+              <td>Periode</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>S2</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>
+          </tbody>
+        </tr>
+      </table>
+      <p>Apakah alasan anda memilih jurusan pada Perguruan Tinggi tersebut?</p>
+      <p><i><strong>Karena saya ingin menjadi programmer</strong></i></p>
+      <p>Apa judul karya tulis ilmiah/skripsi/thesis Anda?</p>
+      <p><i><strong>Aplikasi Pengaturan Jaringan berbasis Teknologi Web</strong></i></p><br/>
+
+      <table class="table table-bordered">
+        <h4>2. Pendidikan Non Formal</h4>
+        <thead>
+          <tr>
+            <th>Nama Kursus atau Pelatihan</th>
+            <th>Penyelenggara</th>
+            <th>Tahun</th>
+            <th>Keterangan</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+
+        <table class="table table-bordered">
+        <h4>3. Kegiatan Sosial</h4>
+        <thead>
+          <tr>
+            <th>Nama Organisasi</th>
+            <th>Bidang</th>
+            <th>Periode Keikutsertaan</th>
+            <th>Keterangan</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table class="table table-bordered">
+        <h4>4. Penghargaan</h4>
+        <thead>
+          <tr>
+            <th>Penghargaan</th>
+            <th>Tahun</th>
+            <th>Keterangan</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table class="table table-bordered">
+        <h4>5. Kemampuan Berbahasa</h4>
+        <thead>
+          <tr>
+            <th>Bahasa Asing yang Dikuasai</th>
+            <th>Lisan</th>
+            <th>Tulisan</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+
+    <div class="list-group">
+      <a class="list-group-item" data-toggle="collapse" href="#data-pekerjaan">IV. Pengalaman Kerja</a>
+    </div>
+    <div id="data-pekerjaan" class="collapse container">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>1. Nama Perusahaa</th>
+            <td>Indomaret</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Bidang Usaha</th>
+            <td>Jual Beli</td>
+          </tr>
+          <tr>
+            <th>Jabatan</th>
+            <td>Kasir</td>
+          </tr>
+          <tr>
+            <th>Periode</th>
+            <td>2014/2015</td>
+          </tr>
+          <tr>
+            <th>Alasan Berhenti</th>
+            <td>Ingin tempat yang lebih baik</td>
+          </tr>
+          <tr>
+            <th colspan=2>Referensi</th>
+          </tr>
+          <tr>
+            <th>Nama</th>
+            <td>Ifnu Bima</td>
+          </tr>
+          <tr>
+            <th>Jabatan</th>
+            <td>Kasir</td>
+          </tr>
+          <tr>
+            <th>No Telepon</th>
+            <td>089660553886</td>
+          </tr>
+          <tr>
+            <th colspan=2>Struktur Organisasi</th>
+          </tr>
+          <tr>
+            <td colspan=2 rowspan=5 >Nanti diisikan data/gambar</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="list-group">
+      <a class="list-group-item" data-toggle="collapse" href="#data-konsep">V. Minat dan Konsep Pribadi</a>
+    </div>
+    <div id="data-konsep" class="collapse container">
+      <p>Mengapa Anda berminat bekerja di PT. Global Digital Niaga(blibli.com) ?</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Mengapa Anda ingin bekerja pada jabatan yang Anda lamar?</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Faktor apa yang akan mendorong Anda untuk bertahan dalam suatu pekerjaan?</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Lingkungan kerja seperti apa yang Anda harapkan?</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Tuliskan value hidup anda!</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Sebutkan keterampilan khusus yang Anda miliki!</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Sebutkan hobi/kegemaran Anda!</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Deskripsikan informasi lain mengenai diri Anda pada kolom ini!</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+    </div>
+
+    <div class="list-group">
+      <a class="list-group-item" data-toggle="collapse" href="#data-lain">VI. Lain-Lain</a>
+    </div>
+    <div id="data-lain" class="collapse container">
+      <p>Dimana Anda memperoleh informasi mengenai lowongan pekerjaan di PT. Global Digital Niaga(blibli.com)?</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Apakah Anda memiliki keluarga, kerabat, atau teman yang bekerja di PT. Global Digital Niaga (blibli.com)?</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Apakah Anda pernah melamar ke PT. Global Digital Niaga (blibli.com) sebelumnya?</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Apakah Anda memiliki pekerjaan sampingan/part time? Sebutkan!</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+      <p>Jika Anda diterima, kapan Anda dapat mulai bekerja ?</p>
+      <p><strong><i>Karena saya ingin berkembang lebih jauh lagi</i></strong></p><br/>
+    </div>
+
+      <label>Comment</label>
+      <textarea name="Text1" cols="140" rows="8" class="form-control" ></textarea>
+       <br/>
+
+    <button type="submit" class="btn btn-primary" name="">Accept CV</button>
+
+    <button type="reset" class="btn btn-warning" name="">Reject</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'view-cv',
+  props: ['department']
+}
+</script>
+
+<style scoped>
+</style>
