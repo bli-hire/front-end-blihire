1297e06cf1cceea1575ebb11a075f1eb27b8558b
igprad <ign.aldi.pradana@gmail.com>
2017-07-21 18:20:26 +0700

    Added Method befoure mount to get total fpk requested

--- src/components/department/Dashboard.vue
+++ src/components/department/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="content">
-    <BoxComponent v-for="n in 4" v-bind:title="content" message="Please we need ..."></BoxComponent>
+    <BoxComponent v-for="n in total" v-bind:title="content" message="Please we need ..."></BoxComponent>
+    <h2 class="msg-empty" v-if="total === 0">There are no new {{content}} requested</h2>
   </div>
 </template>
 
@@ -12,7 +13,24 @@ export default {
     BoxComponent
   },
   name: 'department-dashboard',
-  props: ['content']
+  data () {
+    return {
+      total: 0
+    }
+  },
+  props: ['content'],
+  beforeMount () {
+    var self = this
+    if (this.content === 'FPK') {
+      self.$http.get('http://localhost:8080/internal/fpk').then(response => {
+        var result = JSON.stringify(response)
+        if (JSON.stringify(response.data === '')) {
+          console.log(result)
+          this.total = 0
+        }
+      })
+    }
+  }
 }
 </script>
 
