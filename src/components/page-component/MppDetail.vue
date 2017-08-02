16c7d715ad704c0de73839eb3a29b769d062a6a2
Juan Christian Tjandra <juan.christian@ti.ukdw.ac.id>
2017-08-02 20:17:17 +0700

    mpp detail

--- src/components/page-component/MppDetail.vue
+++ src/components/page-component/MppDetail.vue
@@ -4,12 +4,12 @@
     <p>Position : {{position}}</p>
     <p>Number : {{number}}</p>
     <p>Reason : {{reason}}</p>
-    <p>Main Responsibilty : {{main-responsibilty}}</p>
+    <p>Main Responsibilty : {{mainResponsibilty}}</p>
     <h3>Qualification</h3>
     <p>Education : {{education}}</p>
-    <p>Knowledge Skill : {{knowledge-skill}}</p>
+    <p>Knowledge Skill : {{knowledgeSkill}}</p>
     <p>Experience : {{experience}}</p>
-    <p>Employement Status : {{employement-status}}</p>
+    <p>Employement Status : {{employementStatus}}</p>
     <h3>Expected to Join in {{year}}</h3>
     <table class="table">
           <tbody><tr>
@@ -42,8 +42,8 @@
           </tr>
         </tbody>
       </table>
-      <p>Computer Needed : {{computers}}</p>
-      <p>Spesification : {{spesification}}</p>
+      <p>Computer Needed : {{pcAmmount}}</p>
+      <p>Spesification : {{pcSpec}}</p>
       <h3>Comment :</h3>
       <textarea name="Text1" cols="140" rows="8"></textarea>
   </div>
@@ -52,8 +52,72 @@
 <script>
 export default{
   name: 'mpp-detail',
-  props: ['title', 'position', 'number', 'reason', 'main-responsibilty', 'education', 'experience', 'knowledge-skill', 'employement-status', 'month-1', 'month-2', 'month-3', 'month-4', 'month-5', 'month-6', 'month-7', 'month-8', 'month-9', 'month-10', 'month-11', 'month-12', 'computers', 'spesification', 'year'
-  ]
+  // props: ['title', 'position', 'number', 'reason', 'main-responsibilty', 'education', 'experience', 'knowledge-skill', 'employement-status', 'month-1', 'month-2', 'month-3', 'month-4', 'month-5', 'month-6', 'month-7', 'month-8', 'month-9', 'month-10', 'month-11', 'month-12', 'computers', 'spesification', 'year'
+  // ]
+  data () {
+    return {
+      position: '',
+      department: '',
+      number: '',
+      mainResponsibilty: '',
+      dateCreated: '',
+      reason: '',
+      knowledgeSkill: '',
+      employementStatus: '',
+      education: '',
+      experience: '',
+      pcAmmount: '',
+      // dateNeeded: '',
+      username: '',
+      pcSpec: '',
+      role: '',
+      idSelector: '',
+      idUser: ''
+    }
+  },
+  beforeMount () {
+    var self = this
+    this.idSelector = self.$route.params.id
+    self.username = JSON.parse(window.sessionStorage.getItem('user')).name
+    self.role = JSON.parse(window.sessionStorage.getItem('user')).role
+    self.idUser = JSON.parse(window.sessionStorage.getItem('user')).id
+    if (self.role.includes('Department')) {
+      var roleUrl = 'department'
+    }
+    self.$http.get('http://localhost:8080/mpp/' + this.idSelector).then(response => {
+      var mpp = JSON.stringify(response.data.data)
+      var objMpp = {}
+      objMpp = JSON.parse(mpp)[0]
+      this.department = objMpp.department
+      this.position = objMpp.position
+      this.number = objMpp.numberOfPerson
+      this.reason = objMpp.reason
+      this.employementStatus = objMpp.employeeStatus
+      this.education = objMpp.education
+      this.experience = objMpp.experience
+      this.knowledgeSkill = objMpp.knowledge
+      this.pcSpec = objMpp.pcSpec
+      this.pcAmmount = objMpp.pcAmmount
+      // this.dateNeeded = objMpp.dateNeeded
+      this.dateCreated = objMpp.createdDate
+    }, () => {
+      alert('No Valid Mpp for this id')
+      this.$router.push('/' + roleUrl + '/')
+    }).catch((e) => {
+      alert('No Valid Mpp for this id')
+      this.$router.push('/' + roleUrl + '/')
+    })
+  },
+  methods: {
+    ceoApprove () {
+      this.$http.post('http://localhost:8080/mpp/approve', {
+        idUser: this.idUser,
+        idMpp: parseInt(this.idSelector)
+      }, (json) => {
+        alert(JSON.stringify(json.message))
+      })
+    }
+  }
 }
 </script>
 
