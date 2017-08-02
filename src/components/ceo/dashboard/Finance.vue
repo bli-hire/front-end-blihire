394ef7db451a6d0f9ef44f37b5007b1341a16757
Juan Christian Tjandra <juan.christian@ti.ukdw.ac.id>
2017-08-02 19:54:47 +0700

    mpp list CEO

--- src/components/ceo/dashboard/Finance.vue
+++ src/components/ceo/dashboard/Finance.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="content">
 
-    <BoxComponent 
-      v-if="content === 'fpk'" 
-      v-for="fpk in JSON.parse(resultContent.resultFpk)" 
-      v-bind:title="fpk.department" 
-      v-bind:message="'Reason : '+fpk.reason"
-      v-bind:statusAccept="fpk.accept"
-      v-bind:statusReject="fpk.reject"
-      v-bind:loginStatus="role"
-      v-bind:content="content"
-      v-bind:id="fpk.idFpk"></BoxComponent>
+    <div v-if="content === 'fpk'">
+      <BoxComponent
+
+        v-for="fpk in JSON.parse(resultContent.resultFpk)"
+        v-bind:title="fpk.department"
+        v-bind:message="'Reason : '+fpk.reason"
+        v-bind:statusAccept="fpk.accept"
+        v-bind:statusReject="fpk.reject"
+        v-bind:loginStatus="role"
+        v-bind:content="content"
+        v-bind:id="fpk.idFpk">
+      </BoxComponent>
+    </div>
+    <div v-if="content === 'mpp'">
+      <BoxComponent
+
+        v-for="mpp in JSON.parse(resultContent.resultMpp)"
+        v-bind:title="mpp.department"
+        v-bind:message="'Reason : '+mpp.reason"
+        v-bind:statusAccept="mpp.isAccept"
+        v-bind:statusReject="mpp.isReject"
+        v-bind:loginStatus="role"
+        v-bind:content="content"
+        v-bind:id="mpp.id">
+      </BoxComponent>
+    </div>
 
     <h2 class="msg-empty" v-if="content === 'fpk' && JSON.parse(resultContent.resultTotalFpk) === 0">There are no new {{content}} requested</h2>
 
@@ -61,6 +77,21 @@ export default {
           var totalFpk = JSON.stringify(response.data.totalData)
           this.resultContent.resultFpk = fpk
           this.resultContent.resultTotalFpk = totalFpk
+        }
+      })
+    } else if (this.content === 'mpp') {
+      self.$http.get('http://localhost:8080/mpp/byDepartment/active', {}, {
+        headers: {
+          'department': division
+        }
+      }).then(response => {
+        if (response.data.data === '[]') {
+          this.resultContent.resultTotalMpp = 0
+        } else {
+          var mpp = JSON.stringify(response.data.data)
+          var totalMpp = JSON.stringify(response.data.totalData)
+          this.resultContent.resultMpp = mpp
+          this.resultContent.resultTotalMpp = totalMpp
         }
       })
     }
