947e03a40d15ad7bb6a3638a1d935cf69656a588
Juan Christian Tjandra <juan.christian@ti.ukdw.ac.id>
2017-08-05 23:35:53 +0700

    MppAccepted pending and rejected fixing bind data

--- src/components/department/MppPending.vue
+++ src/components/department/MppPending.vue
@@ -6,7 +6,7 @@
 	    <BoxComponent
 	      v-for="mpp in JSON.parse(resultContent.resultMpp)"
 	      v-bind:title="mpp.department"
-	      v-bind:message="'Reason : '+mpp.reason"
+	      v-bind:message="'Reason : '+mpp.mppDetails.reason"
 	      v-bind:statusAccept="mpp.isAccept"
 	      v-bind:statusReject="mpp.isReject"
 	      v-bind:loginStatus="role"
