ae370d01c1baeb12ef96c48d41a41c44a359bf6e
igprad <ign.aldi.pradana@gmail.com>
2017-08-09 01:57:39 +0700

    Added number of essay,multiple choice, hard,easy,medium queston input number for technical test selection

--- src/components/hrd/HiringProcess.vue
+++ src/components/hrd/HiringProcess.vue
@@ -7,64 +7,95 @@
     <div class="list-group">
       <a class="list-group-item" data-toggle="collapse" href="#Interview1">Interview 1</a>
     </div>
-    <div id="Interview1" class="collapse container">
-        Tanggal : <input type="date" name="tanggal"><br/><br/>
-        PIC : <select id="listofHR">
+    <div id="Interview1" class="collapse">
+        <label for="">Tanggal : </label>
+        <input type="date" name="tanggal"><br/><br/>
+        <label for="">PIC : </label><select id="listofHR">
           <option>Chandra Arko</option>
           <option>Clara</option>
           <option>Eunike</option>
         </select><br/><br/>
+        <button class="btn btn-primary">Send</button>
+        <br>
+        <br>
     </div>
-
+    
     <div class="list-group">
       <a class="list-group-item" data-toggle="collapse" href="#Interview2">Interview 2</a>
     </div>
-    <div id="Interview2" class="collapse container">
-        Tanggal : <input type="date" name="tanggal"><br/><br/>
-        PIC : <select id="listofHR">
+    <div id="Interview2" class="collapse">
+        <label for="">Tanggal : </label>
+        <input type="date" name="tanggal"><br/><br/>
+        <label for="">PIC : </label>
+        <select id="listofHR">
           <option>Chandra Arko</option>
           <option>Clara</option>
           <option>Eunike</option>
         </select><br/><br/>
+        <button class="btn btn-primary">Send</button>
+        <br>
+        <br>
     </div>
 
     <div class="list-group">
       <a class="list-group-item" data-toggle="collapse" href="#TechnicalTest">Technical Test</a>
     </div>
-    <div id="TechnicalTest" class="collapse container">
-        Tanggal : <input type="date" name="tanggal"><br/><br/>
-        PIC : <select id="listofHR">
+    <br>
+    <div id="TechnicalTest" class="collapse">
+        <label for="">Tanggal : </label>
+        <input type="date" name="tanggal"><br/><br/>
+        <label for="">PIC : </label>
+        <select id="listofHR">
           <option>Chandra Arko</option>
           <option>Clara</option>
           <option>Eunike</option>
         </select><br/><br/>
+        <div class="technicalQuestion">
+          <label for="">Technical Test :</label><br>
+          <label for="">Essay Question:</label><input type="number"><br>
+          <label for="">Medium Question:</label><input type="number"><br>
+          <label for="">Hard Question:</label><input type="number"><br>
+          <label for="">Mutliple Choice:</label><input type="number"><br>
+          <label for="">Essay</label><input type="number"><br>
+        </div>
+        <br>
+        <button class="btn btn-primary">Send</button>
+        <br>
+        <br>
     </div>
 
     <div class="list-group">
       <a class="list-group-item" data-toggle="collapse" href="#PsychoTest">Psikotes</a>
     </div>
-    <div id="PsychoTest" class="collapse container">
-        Tanggal : <input type="date" name="tanggal"><br/><br/>
-        PIC : <select id="listofHR">
+    <div id="PsychoTest" class="collapse">
+        <label for="">Tanggal : </label><input type="date" name="tanggal"><br/><br/>
+        <label for="">PIC : </label><select id="listofHR">
           <option>Chandra Arko</option>
           <option>Clara</option>
           <option>Eunike</option>
         </select><br/><br/>
+        <button class="btn btn-primary">Send</button>
+        <br>
+        <br>
      </div>
 
      <div class="list-group">
       <a class="list-group-item" data-toggle="collapse" href="#medicalCheckup">Medical Checkup</a>
     </div>
-    <div id="medicalCheckup" class="collapse container">
-        Tanggal : <input type="date" name="tanggal"><br/><br/>
-        PIC : <select id="listofHR">
+
+    <div id="medicalCheckup" class="collapse">
+        <label for="">Tanggal : </label><input type="date" name="tanggal"><br/><br/>
+        <label for="">PIC : </label><select id="listofHR">
           <option>Chandra Arko</option>
           <option>Clara</option>
           <option>Eunike</option>
         </select><br/><br/>
+        <button class="btn btn-primary">Send</button>
+        <br>
+        <br>
      </div>
 
-     <input type="submit" class="btn btn-primary" value="OK"/>
+     <!-- <input type="submit" class="btn btn-primary" value="OK"/> -->
       <p class="hint"><i>*Notifikasi akan langsung terkirimkan ke email kandidat dan PIC yang terpilih</i></p>
     </form>
   </div>
@@ -78,4 +109,10 @@ export default {
 </script>
 
 <style scoped>
+label {
+  margin-right: 0.4vw;
+}
+.technicalQuestion {
+  text-align: center;
+}
 </style>
